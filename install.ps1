3e67984c622542f8fbc0d8f587867f4dbb0ec621
Christer Eriksson <christer@dogtail.se>
2019-03-15 08:44:27 +0100

    First try

---
+++ install.ps1
@@ -0,0 +1,10 @@
+choco install docker-desktop 
+choco install vscode 
+choco install azure-data-studio 
+choco install gitkraken 
+choco install fiddler 
+choco install postman
+choco install wsl-ubuntu-1804  
+choco install sql-server-management-studio 
+choco install visualstudio2017enterprise 
+choco install visualstudio2019enterprise --pre 
